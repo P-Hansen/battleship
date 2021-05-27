fdea542c30695e0e22ee71911cdd6d0985037cb5
P-Hansen <philip.pt.hansen@gmail.com>
2021-05-26 23:43:28 -0600

    placing 5 and 6 letter words

--- clicks.js
+++ clicks.js
@@ -49,7 +49,7 @@ const userInput = function(board) {
       let col = $(this).attr("class")[0];
       let row = $(this).parent().attr("class");
       
-      let col4 = $(this).next().attr("class")[0];
+      let col4 = $(this).next().next().next().attr("class")[0];
       board[col4+row] = fourWord[3];
       $(this).next().next().next().text(fourWord[3]);
 
@@ -70,6 +70,71 @@ const userInput = function(board) {
       $(this).next().next().next().addClass("placed");
 
       gameState = null;
+    } else if (gameState === "place5") {
+      let col = $(this).attr("class")[0];
+      let row = $(this).parent().attr("class");
+
+      let col5 = $(this).next().next().next().next().attr("class")[0];
+      board[col5+row] = fiveWord[4];
+      $(this).next().next().next().next().text(fiveWord[4]);
+      
+      let col4 = $(this).next().next().next().attr("class")[0];
+      board[col4+row] = fiveWord[3];
+      $(this).next().next().next().text(fiveWord[3]);
+
+      let col3 = $(this).next().next().attr("class")[0];
+      board[col3+row] = fiveWord[2];
+      $(this).next().next().text(fiveWord[2]);
+
+      let col2 = $(this).next().attr("class")[0];
+      board[col2+row] = fiveWord[1];
+      $(this).next().text(fiveWord[1]);
+      
+      board[col+row] = fiveWord[0];
+      $(this).text(fiveWord[0]);
+      
+      $(this).addClass("placed");
+      $(this).next().addClass("placed");
+      $(this).next().next().addClass("placed");
+      $(this).next().next().next().addClass("placed");
+      $(this).next().next().next().next().addClass("placed");
+
+      gameState = null;
+    } else if (gameState === "place6") {
+      let col = $(this).attr("class")[0];
+      let row = $(this).parent().attr("class");
+
+      let col6 = $(this).next().next().next().next().next().attr("class")[0];
+      board[col6+row] = sixWord[5];
+      $(this).next().next().next().next().next().text(sixWord[5]);
+
+      let col5 = $(this).next().next().next().next().attr("class")[0];
+      board[col5+row] = sixWord[4];
+      $(this).next().next().next().next().text(sixWord[4]);
+      
+      let col4 = $(this).next().next().next().attr("class")[0];
+      board[col4+row] = sixWord[3];
+      $(this).next().next().next().text(sixWord[3]);
+
+      let col3 = $(this).next().next().attr("class")[0];
+      board[col3+row] = sixWord[2];
+      $(this).next().next().text(sixWord[2]);
+
+      let col2 = $(this).next().attr("class")[0];
+      board[col2+row] = sixWord[1];
+      $(this).next().text(sixWord[1]);
+      
+      board[col+row] = sixWord[0];
+      $(this).text(sixWord[0]);
+      
+      $(this).addClass("placed");
+      $(this).next().addClass("placed");
+      $(this).next().next().addClass("placed");
+      $(this).next().next().next().addClass("placed");
+      $(this).next().next().next().next().addClass("placed");
+      $(this).next().next().next().next().next().addClass("placed");
+
+      gameState = null;
     }
   });
 
@@ -86,12 +151,19 @@ const userInput = function(board) {
         $(this).next().addClass("place");
         $(this).next().next().addClass("place");
         $(this).next().next().next().addClass("place");
-        // let colClass = $(this).attr("class")[0];
-        // let rowClass = $(this).parent().attr("class");
-        // console.log("col?", colClass);
-        // console.log("row?", rowClass);
-        //
-        // $(this).next(colClass).next(colClass).addClass("place");
+      } else if (gameState === "place5") {
+        $(this).addClass("place");
+        $(this).next().addClass("place");
+        $(this).next().next().addClass("place");
+        $(this).next().next().next().addClass("place");
+        $(this).next().next().next().next().addClass("place");
+      } else if (gameState === "place6") {
+        $(this).addClass("place");
+        $(this).next().addClass("place");
+        $(this).next().next().addClass("place");
+        $(this).next().next().next().addClass("place");
+        $(this).next().next().next().next().addClass("place");
+        $(this).next().next().next().next().next().addClass("place");
       }
     });
 
@@ -108,6 +180,19 @@ const userInput = function(board) {
         $(this).next().removeClass("place");
         $(this).next().next().removeClass("place");
         $(this).next().next().next().removeClass("place");
+      } else if (gameState === "place5") {
+        $(this).removeClass("place");
+        $(this).next().removeClass("place");
+        $(this).next().next().removeClass("place");
+        $(this).next().next().next().removeClass("place");
+        $(this).next().next().next().next().removeClass("place");
+      } else if (gameState === "place6") {
+        $(this).removeClass("place");
+        $(this).next().removeClass("place");
+        $(this).next().next().removeClass("place");
+        $(this).next().next().next().removeClass("place");
+        $(this).next().next().next().next().removeClass("place");
+        $(this).next().next().next().next().next().removeClass("place");
       }
 
     });
@@ -130,6 +215,20 @@ const userInput = function(board) {
   $("#4-letter").on("click", function(event) {
     gameState = "place4";
     console.log("placing 4 letter word");
+    $(this).remove();
+    });
+
+    //click 5 letter word
+  $("#5-letter").on("click", function(event) {
+    gameState = "place5";
+    console.log("placing 5 letter word");
+    $(this).remove();
+    });
+
+    //click 6 letter word
+  $("#6-letter").on("click", function(event) {
+    gameState = "place6";
+    console.log("placing 6 letter word");
     $(this).remove();
     });
 /////////////////////////////////////////////////////////////////////
